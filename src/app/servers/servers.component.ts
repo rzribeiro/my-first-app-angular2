2ad4372baecc42dc9b21ec4005387f31a39eb798
Rodrigo Zanella Ribeiro <rodrigo.ribeiro@holdprint.com.br>
2018-07-06 22:57:51 -0300

    Várias formas de definir um selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+   selector: 'app-servers', // You should call in html: <app-servers> </app-servers>
+  // selector: '[app-servers]', You should call in html: <div app-servers>
+  // selector: '.app-servers', You should call in html: <div class="app-servers">
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
